91abbe23c31f7914d82e2b11af592481f4363a29
Syed Furqan Ahmed <sonicfurqan@gmail.com>
2024-02-26 23:42:17 +0530

    completing basic s

--- basicProblems.js
+++ basicProblems.js
@@ -22,4 +22,118 @@ const countDigits = (numb) => {
     console.log(number, divideByItSelfCounter);
 
 }
+//convert the given number to 32 bits and inverse
+const reverseBits = (numb) => {
+    let bitsNumb = numb.toString(2);
+    let fullUnsigned = bitsNumb.padStart(32, '0');
+    let reverseUnsigned = '';
+    for (let i = fullUnsigned.length - 1; i > 0; i--) {
+        reverseUnsigned += fullUnsigned[i];
+    }
 
+    console.log(fullUnsigned);
+    console.log(reverseUnsigned);
+    console.log(parseInt(fullUnsigned, 2));
+    console.log(parseInt(reverseUnsigned, 2));
+}
+
+//Check whether a given number ’n’ is a palindrome number.
+
+const checkPalindrome = (num) => {
+
+    let value = num;
+
+    let reverseValue = '';
+
+    while (value > 0) {
+        reverseValue += value % 10;
+        value = parseInt(value / 10);
+        console.log(value)
+
+    }
+    if (num == parseInt(reverseValue)) {
+        console.log('Palindrome');
+    }
+    else {
+        console.log('No');
+    }
+
+
+}
+
+const checkGCD = (num1, num2) => {
+
+    let loopLimit = num2;
+    if (num1 < num2) {
+        loopLimit = num1;
+    }
+    let dividingNumber = 0;
+    for (let i = 1; i <= loopLimit; i++) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            dividingNumber = i;
+        }
+    }
+    console.log(dividingNumber);
+
+}
+
+
+const checkAmstrong = (num) => {
+
+    let value = num;
+
+    let numLength = (num + '').length;
+
+    let resultNum = 0;
+
+    while (value > 0) {
+
+        let reminder = value % 10;
+        let powerValue = reminder;
+        for (let i = 1; i <= numLength - 1; i++) {
+            powerValue = powerValue * reminder;
+        }
+        resultNum += powerValue;
+
+        value = parseInt(value / 10);
+        console.log(value);
+    }
+    console.log(num);
+    console.log(resultNum);
+    if (num == resultNum) {
+        console.log('Amstrong')
+    }
+    else {
+        console.log('No');
+    }
+
+}
+
+
+const sumOfDivisors = (num) => {
+
+    let result = 0;
+    for (let i = 1; i <= num; i++) {
+        console.log(i);
+        console.log(num % i);
+        if (num % i == 0) {
+            console.log(i);
+            result += i;
+        }
+    }
+    console.log(result);
+}
+
+const primeNumber = (num) => {
+
+    let isPrime = true;
+    for (let i = 2; i <= num - 1; i++) {
+
+        if (num % i == 0) {
+            i = num;
+            isPrime = false;
+        }
+    }
+
+    console.log(isPrime);
+}
